416e2ae8c4f52096120693201a3681d252881ee2
Leandro Gonçalves <leandro.gwn@gmail.com>
2024-07-08 00:54:58 -0300

    Estudo de implementação usando a documentação do flask e do sqlalchemy.

--- src/app.py
+++ src/app.py
@@ -1,15 +1,43 @@
-from flask import Flask
+import os
 
-app = Flask(__name__)
+from flask import Flask, current_app
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase
+import click
 
-@app.route("/olamundo/<usuario>/<int:idade>/<float:altura>/")
-def hello_world(usuario, idade, altura):
-    return {
-        "Usuario":usuario,
-        "Idade":idade,
-        "Altura":altura,
-    }
+class Base(DeclarativeBase):
+  pass
 
-@app.route("/bemvindo")
-def bem_vindo():
-    return "<h1>Bem vindo!</h1>"
+db = SQLAlchemy(model_class=Base)
+
+@click.command('init-db')
+def init_db_command():
+    """Clear the existing data and create new tables."""
+    global db
+    with current_app.app_context():
+        db.create_all()
+    click.echo('Initialized the database.')
+
+
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        SQLALCHEMY_DATABASE_URI='sqlite:///lgbank.sqlite',
+    )
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # TRegistir cli commands
+    app.cli.add_command(init_db_command)
+
+    # Initialize extensions
+    db.init_app(app)
+
+    return app
